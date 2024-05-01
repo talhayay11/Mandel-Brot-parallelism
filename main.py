b4bdb5723835ff7ec34e3461f91f6d7209e97db7
Berhan-hub <72172084+Berhan-hub@users.noreply.github.com>
2024-05-01 22:52:46 +0300

    resolution selector
    added

--- main.py
+++ main.py
@@ -16,13 +16,20 @@ from mandelbrot_core import processCount
 #     plot_mandelbrot_gpu(max_iter=10000, width=1024, height=1024)
 #     plot_zoomable_mandelbrot(1000)
 
+def on_resolution_selected(selected_resolution):
+    print("Selected resolution:", selected_resolution)
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Mandelbrot Set Viewer")
-    button = tk.Button(root, text="Click Me")
-    button.pack(pady=20)
-    #mandelbrot_display = ZoomableMandelbrot(ax, max_iter=100,regions="auto")
+    resolutions = ["800x600", "1024x768", "1280x720", "1920x1080"]
+    selected_resolution = tk.StringVar(root)
+    selected_resolution.set(resolutions[0])  # Default resolution
+    
+    # Dropdown menu for selecting resolution
+    resolution_dropdown = tk.OptionMenu(root, selected_resolution, *resolutions, command=on_resolution_selected)
+    resolution_dropdown.pack(pady=20)
+    
     for processors in processCount:
         mandelbrot_display =  ZoomableMandelbrot(root, max_iter=100,regions="auto", processors=processors)
         #plt.show()
