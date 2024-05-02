3c1ba7540d981531149a66edf6012c7eec1bd594
lntreter <111744121+lntreter@users.noreply.github.com>
2024-05-02 13:46:28 +0300

    resolution fixed

--- main.py
+++ main.py
@@ -15,19 +15,24 @@ maximumPhysicalCores = os.cpu_count() // 2
 
 minToMaxProcessors = list(range(1,maximumPhysicalCores+1))
 
- 
 
 # if __name__ == '__main__':
     
 #     plot_mandelbrot_gpu(max_iter=10000, width=1024, height=1024)
 #     plot_zoomable_mandelbrot(1000)
 
-def on_resolution_selected():
-    # width, height = map(int, selected_resolution.split('x'))
-    # print(width, height)
+def on_resolution_selected(selected_resolution):
+    global resolution
+    resolution = selected_resolution
+    print(f"Selected resolution: {resolution}")
+    
+
+def ComputeAll():
+    width, height = map(int, resolution.split('x'))
+    print(width, height)
     print(f"Testing for {minToMaxProcessors} processors")
     for processors in minToMaxProcessors:
-        mandelbrot = ZoomableMandelbrot(root, max_iter=100, regions="auto", processors=processors, width=800, height=800, ComputeOnce=False)
+        mandelbrot = ZoomableMandelbrot(root, max_iter=100, regions="auto", processors=processors, width=width, height=height, ComputeOnce=False)
         # Do further operations with mandelbrot_display if needed
     
     print(f"Efficiency: {Efficiency}")
@@ -42,8 +47,9 @@ def on_resolution_selected():
     Speedup.clear()
 
 def ComputeOnce():
+    width, height = map(int, resolution.split('x'))
 
-    ZoomableMandelbrot(root, max_iter=100, regions="auto", processors=maximumPhysicalCores, width=800, height=800, ComputeOnce=True)
+    ZoomableMandelbrot(root, max_iter=100, regions="auto", processors=maximumPhysicalCores, width=width, height=height, ComputeOnce=True)
         # Do further operations with mandelbrot_display if needed
 
 if __name__ == '__main__':
@@ -56,7 +62,7 @@ if __name__ == '__main__':
 
     button = tk.Button(root, text="Tüm Çekirdeklerle Hesapla", command=ComputeOnce)
     button.pack(pady=20)  # Add some vertical padding
-    button = tk.Button(root, text="Sırayla Tüm Çekirdekler", command=on_resolution_selected)
+    button = tk.Button(root, text="Sırayla Tüm Çekirdekler", command=ComputeAll)
     button.pack(pady=20)  # Add some vertical padding
 
     # Dropdown menu for selecting resolution
